d195582c11dee682e39521cab0f87fdf275dd83b
Sabre <62978529+sabrereyn@users.noreply.github.com>
2021-07-02 14:45:18 -0700

    Added Userform and logic

---
+++ src/NewUser/UserForm.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const UserForm = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      Username: enteredUsername,
+      Age: enteredAge,
+    };
+
+    console.log(newUser);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          ></input>
+        </div>
+        <div>
+          <label>Age (Years)</label>
+          <input
+            type="text"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
+        </div>
+        <div>
+          <button type="submit">Add User</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default UserForm;
